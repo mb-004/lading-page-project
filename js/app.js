86825af9e11bacd6d9c6e87c5e9592a70f086abb
Marta Borges <martaborges.004@campus.masterschool.com>
2022-10-07 14:27:17 +0100

    Create NavBar

--- js/app.js
+++ js/app.js
@@ -1,62 +1,29 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+document.addEventListener('DOMContentLoaded', () => {
+//    console.log('DOM has been fully loaded')
+    const selectNav = document.querySelector("#navbar__list");
+    //console.log("selectNav", selectNav);
+    const createList = document.createElement("li");
+    //console.log("createList", createList);
+    createList.setAttribute("class", "menu__link");
+    //console.log("createList", createList);
+/* <----------- Creating an Li on the NavBar -----------------> */   
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+    const createNavAnchor = document.createElement("a");
+    //console.log("createNavAnchor", createNavAnchor);
+    createNavAnchor.setAttribute("href", "#section4");
+    //console.log("createNavAnchor", createNavAnchor);
+    const selectNavText = document.querySelector("h2");
+    //console.log("selectNavText", selectNavText);
+    createList.textContent = selectNavText.textContent;
+    createNavAnchor.innerHTML = selectNavText.textContent;
+    //console.log("createNavAnchor", createNavAnchor);
+/* <----------- Creating an <a> with the HTML text from H2 title section --------------> */
 
-/**
- * Define Global Variables
- * 
-*/
+    createList.innerHTML = createNavAnchor.outerHTML;
+    //console.log("createList", createList);
+/* <------------- Setting Li Content = <a> with HTML text from h2 title section ---------> */
 
+    selectNav.appendChild(createList);
+/* <------------- Appending <li> to the <nav> NavBar ---------------> */
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+})
